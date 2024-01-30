b2de543f149fd05ec9e216372ec1e693e44ae4f7
TheOmegaCarrot <theomegacarrot@gmail.com>
2024-01-30 16:37:45 -0500

    unbork cmake again

--- cmake/testing.cmake
+++ cmake/testing.cmake
@@ -1,4 +1,5 @@
 function(register_test input_test_file test_name)
+  set(TEST_BIN_DIR ${CMAKE_CURRENT_BINARY_DIR}/tests)
 
   add_executable(${test_name} ${input_test_file})
 
@@ -6,24 +7,20 @@ function(register_test input_test_file test_name)
 
   target_include_directories(${test_name} PRIVATE ${TOP_DIR}/cpp/include/)
 
-  set_target_properties(${test_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY
-    ${CMAKE_BINARY_DIR}/tests)
+  set_target_properties(${test_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${TEST_BIN_DIR})
 
   target_compile_features(${test_name} PUBLIC cxx_std_20)
 
   add_test(
     NAME ${test_name}
     COMMAND ${test_name}
-    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/tests)
+    WORKING_DIRECTORY ${TEST_BIN_DIR})
 
 endfunction()
 
 function(test_handling)
-  set(TEST_BIN_DIR ${CMAKE_CURRENT_BINARY_DIR}/tests)
 
   # make ctest give useful output to console on test failure
   list(APPEND CMAKE_CTEST_ARGUMENTS "--output-on-failure" "--progress")
-
-  list(APPEND TEST_STANDARDS "20")
 
 endfunction()
